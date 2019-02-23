e538e757a07321a1b2f5a36aa4bc5b96c5b6dcd9
Elena <litishia@ya.ru>
2019-02-22 22:09:41 -0500

    added navigation

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import StreamList from './streams/StreamList';
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
 import StreamShow from './streams/StreamShow';
+import Header from './Header';
 
 
 
@@ -14,6 +15,7 @@ const App = () => {
     <div className="ui container">
     <BrowserRouter>
       <div>
+        <Header />
         <Route path='/' exact component={StreamList} />
         <Route path='/streams/new' exact component={StreamCreate} />
         <Route path='/streams/edit' exact component={StreamEdit} />
