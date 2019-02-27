6e4b47f7d2421ac64e85f3ebb729e0e2a66ebcca
Elena <litishia@ya.ru>
2019-02-27 14:02:54 -0500

    added streams reducer and lodash lib

---
+++ src/reducers/streamReducer.js
@@ -0,0 +1,19 @@
+import _ from 'lodash';
+
+export default (state={}, action) => {
+  switch (action.type) {
+    case 'FETCH_STREAM':
+      return {state}; //{...state, [action.payload.id]: action.payload}
+    case 'CREATE_STREAM':
+      return {...state, [action.payload.id]: action.payload};
+    case 'EDIT_STREAM':
+      return {...state, [action.payload.id]: action.payload};
+    case 'DELETE_STREAM':
+      let newState = delete state[action.payload];
+      return newState; //avoiding lodash here
+    case 'FETCH_STREAMS':
+      return {...state,..._.mapKeys(action.payload, 'id') }
+    default:
+      return state;
+  }
+};
