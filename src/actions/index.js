bd58783aebbe61580c78feccde766c6eb42ef200
Elena <litishia@ya.ru>
2019-02-27 11:37:22 -0500

    config of axios request to our db api and bulk action crators for all CRUD actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import streams from '../apis/streams';
+
 export const signIn = (userId) => {
   return {
     type: 'SIGN_IN',
@@ -9,4 +11,39 @@ export const signOut = () => {
   return {
     type: 'SIGN_OUT'
   };
+};
+
+export const createStream = (formValues) => {
+  return async (dispatch) => {
+    const response = await streams.post('/streams', formValues);
+    dispatch({ type: 'CREATE_STREAM', payload:response.data })
+  }
+};
+
+export const fetchStreams = () => {
+  return async (dispatch) => {
+    const response = await streams.get('/streams');
+    dispatch({ type: 'FETCH_STREAMS', payload:response.data })
+  }
+};
+
+export const fetchStream = (id) => {
+  return async (dispatch) => {
+    const response = await streams.get(`/streams/${id}`);
+    dispatch({ type: 'FETCH_STREAM', payload:response.data })
+  }
+};
+
+export const editStream = (id , formValues) => {
+  return async (dispatch) => {
+    const response = await streams.put(`/streams/${id}`, formValues);
+    dispatch({ type: 'EDIT_STREAM', payload:response.data })
+  }
+};
+
+export const deleteStream = (id) => {
+  return async (dispatch) => {
+    await streams.delete(`/streams/${id}`);
+    dispatch({ type: 'DELETE_STREAM', payload: id })
+  }
 };
