30e633a32352f624bc26bd635b9c07a8c3cb1548
Elena <litishia@ya.ru>
2019-02-28 13:59:26 -0500

    made url-based selection

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
 
-const StreamEdit = () => {
-  return (
-    <div>StreamEdit</div>
-  )
-};
+class StreamEdit extends React.Component {
 
-export default StreamEdit;
+  componentDidMount() {
+    //console.log(this.props);
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading...</div> // first time component is rendered stream is undefined
+    }
+    return (
+      <div>{this.props.stream.title}</div>
+    )
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return { stream:state.streams[ownProps.match.params.id] }
+}
+
+export default connect(mapStateToProps, { fetchStream })(StreamEdit);
