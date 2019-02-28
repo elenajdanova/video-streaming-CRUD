af95d23ce5aea9bc455596bf11eec694f5285c9c
Elena <litishia@ya.ru>
2019-02-27 23:37:40 -0500

    sdded navigation and edit delete buttons to List component

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchStreams } from '../../actions';
 
 
@@ -8,34 +9,61 @@ class StreamList extends React.Component {
     this.props.fetchStreams();
   }
 
-  renderList() {
-    return this.props.streams.map(stream => {
-      return (
-        <div className="item" key={stream.id}>
-          <i className="large middle aligned icon camera" />
-          <div className="content">
-            {stream.title}
-            <div className="description">{stream.description}</div>
-          </div>
-        </div>
-      );
-    });
+renderAdmin(stream) {
+  if (stream.userId === this.props.currentUserId) {
+    return (
+      <div className="right floated content">
+        <button className="ui button primary"> Edit </button>
+        <button className="ui button negative"> Delete </button>
+      </div>
+    )
   }
+}
+
+renderList() {
+  return this.props.streams.map(stream => {
+    return (
+      <div className="item" key={stream.id}>
+        {this.renderAdmin(stream)}
+        <i className="large middle aligned icon camera" />
+        <div className="content">
+          {stream.title}
+          <div className="description">{stream.description}</div>
+        </div>
+      </div>
+    );
+  });
+}
+
+renderCreateBtn() {
+  if (this.props.isSignedIn) {
+    return (
+      <div style={{ textAlign: "right" }}>
+        <Link to="/streams/new" className="ui button primary">
+          Create Stream
+        </Link>
+      </div>
+    )
+  }
+}
 
   render() {
     return (
       <div>
         <h2>Streams</h2>
-        <div className="ui celled list">
-          {this.renderList()}
-        </div>
+        <div className="ui celled list"> {this.renderList()}</div>
+        {this.renderCreateBtn()}
       </div>
     )
   }
 };
 
 const mapStateToProps = (state) => {
-  return { streams: Object.values(state.streams) } // converting obj to array
+  return {
+    streams: Object.values(state.streams), // converting obj to array
+    currentUserId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn
+  };
 };
 
 export default connect(mapStateToProps, { fetchStreams })(StreamList);
--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -3,7 +3,7 @@ import _ from 'lodash';
 export default (state={}, action) => {
   switch (action.type) {
     case 'FETCH_STREAM':
-      return {state}; //{...state, [action.payload.id]: action.payload}
+      return {...state, [action.payload.id]: action.payload};
     case 'CREATE_STREAM':
       return {...state, [action.payload.id]: action.payload};
     case 'EDIT_STREAM':
