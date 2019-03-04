0374441716a6e7eedafe056623e509cea404e0f3
Elena <litishia@ya.ru>
2019-03-04 11:10:04 -0500

    added modal skafolding for delete stream

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import Modal from '../Modal';
+import history from '../../history';
 
 
 const StreamDelete = () => {
+  const actions = (
+    //div with no css === <> </>
+    <React.Fragment>
+      <button className="ui button negative"> Delete </button>
+      <button className="ui button"> Cancel </button>
+    </React.Fragment>
+  );
   return (
-    <div>StreamDelete</div>
-  )
+    <div>
+    Delete Stream
+     <Modal
+      title="Delete stream"
+      content="Are you sure you want to delete this stream?"
+      actions= {actions}
+      onDismiss={() => history.push('/')}
+    />
+    </div>
+  );
 };
 
 export default StreamDelete;
